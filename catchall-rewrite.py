221bc8400a6af14c07fa6a00c2398bc9a32395ff
Zero <30011708+attackzero@users.noreply.github.com>
2023-05-21 10:19:03 -0400

    This version actually works.

--- catchall-rewrite.py
+++ catchall-rewrite.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import argparse
 import base64
 import binascii
@@ -18,7 +19,96 @@ class MessageHandlerAction(enum.Enum):
     # Modify the outbound message (rewrite the from, to, and reply-to headers)
     MODIFY_OUTBOUND = 3
 
+class CatchAllInfo:
+    def __init__(self, catchall_addr) -> None:
+        self._outbound_addr = None
+        self._recipient_display_name = None
+        self._recipient_username = None
+        self._recipient_domain = None
+        self._valid_info = False
+        self._deconstruct_info(catchall_addr)
+
+    @property
+    def outbound_addr(self):
+        return self._outbound_addr
+    
+    @outbound_addr.setter
+    def outbound_addr(self, addr):
+        self._outbound_addr = addr
+    
+    @property
+    def outbound_username(self):
+        if self._outbound_addr is None:
+            return None
+        else:
+            return self._outbound_addr.split('@')[0]
+    
+    @property
+    def outbound_domain(self):
+        if self._outbound_addr is None:
+            return None
+        else:
+            return self._outbound_addr.split('@')[1]
+    
+    @property
+    def recipient_display_name(self):
+        return self._recipient_display_name
+    
+    @recipient_display_name.setter
+    def recipient_display_name(self, name):
+        self._recipient_display_name = name
+    
+    @property
+    def recipient_username(self):
+        return self._recipient_username
+    
+    @recipient_username.setter
+    def recipient_username(self, username):
+        self._recipient_username = username
+    
+    @property
+    def recipient_domain(self):
+        return self._recipient_domain
+    
+    @recipient_domain.setter
+    def recipient_domain(self, domain):
+        self._recipient_domain = domain
+    
+    @property
+    def recipient_addr(self):
+        if self._recipient_username is None or self._recipient_domain is None:
+            return None
+        else:
+            return f'{self._recipient_username}@{self._recipient_domain}'
+    
+    @property
+    def is_valid(self):
+        return self._valid_info
+
+    def _deconstruct_info(self, addr):
+        try:
+            catchall_recipient = addr.split('@')[0]
+            catchall_recipient_information = catchall_recipient[len('catchall-'):]
+        except:
+            self._valid_info = False
+
+        try:
+            catchall_information_decoded = base64.b64decode(catchall_recipient_information)
+        except (binascii.Error, UnicodeDecodeError):
+            self._valid_info = False
+            return
+        
+        catchall_reply_information = catchall_information_decoded.split(b'\x1e')
+        if len(catchall_reply_information) != 4:
+            # We encoded 4 elements, so we expect 4 elements back
+            self._valid_info = False
+            return
+        
+        (self._outbound_addr, self._recipient_display_name, self._recipient_username, self._recipient_domain) = (part.decode('utf-8') for part in catchall_reply_information)
+        self._valid_info = True
+        
 catchall_recipient_map = dict()
+catchall_domains = list()
 stderr = os.fdopen(sys.stderr.fileno(), 'w', encoding='latin-1', buffering=1)
 
 def debug_msg(msg):
@@ -36,60 +126,62 @@ def read_recipient_map(map_file):
             for line in map_information.readlines():
                 try:
                     # Lines in OpenSMTPd alias files have the format [key]<whitespace>[value]
-                    (catchall_recipient, local_recipient) = line.split()
+                    (catchall_recipient, local_recipient) = line.strip().split()
                     catchall_recipient_map[catchall_recipient] = local_recipient
+                    catchall_domain = catchall_recipient.split('@')[1]
+                    if catchall_domain not in catchall_domains:
+                        catchall_domains.append(catchall_domain)
+                        debug_msg(f'Added catchall domain {catchall_domain}')
                     debug_msg(f'Mapped recipient {catchall_recipient} to {local_recipient}')
                 except ValueError:
                     continue
+
 
 def extract_recipient(session, message_id, result, address):
     """Stores the receipient of the current message in the session."""
     if result != 'ok':
         return
-    if not address.endswith('@catchall.invalid') or address not in catchall_recipient_map:
+    
+    (recipient, recipient_domain) = address.split('@')
+    
+    if recipient_domain not in catchall_domains and recipient.startswith('catchall-'):
+        debug_msg(f'Received outbound mail for catchall: {address}')
+        catchall_info = CatchAllInfo(address)
+        if not catchall_info.is_valid:
+            session['action'] = MessageHandlerAction.PASS
+            return
+        
+        debug_msg(f'Catchall information: FROM: {catchall_info.outbound_addr}; DISP: {catchall_info.recipient_display_name}; TO: {catchall_info.recipient_addr}')
+        
+        session['outbound_recipient'] = email.headerregistry.Address(display_name=catchall_info.recipient_display_name, username=catchall_info.recipient_username, domain=catchall_info.recipient_domain)
+        session['outbound_sender'] = email.headerregistry.Address(display_name='', username=catchall_info.outbound_username, domain=catchall_info.outbound_domain)
+        session['action'] = MessageHandlerAction.MODIFY_OUTBOUND
+    elif recipient_domain in catchall_domains:
+        debug_msg(f'Received message for catchall recipient {address} (mapped to {catchall_recipient_map.get(address)})')
+        session['catchall_recipient'] = address
+        session['local_recipient'] = catchall_recipient_map.get(address)
+        session['action'] = MessageHandlerAction.MODIFY_INBOUND
+    else:
         debug_msg(f'Catchall not processing message for {address}')
         session['action'] = MessageHandlerAction.PASS
-        return
-    
-    if address.endswith('@catchall.invalid'):
-        debug_msg(f'Received outbound mail for catchall: {address}')
-        catchall_information = address[:address.rindex('@catchall.invalid')]
-        try:
-            catchall_information_decoded = base64.b64decode(catchall_information).decode()
-        except (binascii.Error, UnicodeDecodeError):
-            session['action'] = MessageHandlerAction.PASS
-            return
 
-        catchall_reply_information = catchall_information_decoded.split(b'\x1e')
-        if len(catchall_reply_information) != 4:
-            # We encoded 4 elements, so we expect 4 elements back
-            session['action'] = MessageHandlerAction.PASS
-            return
+def rewrite_envelope(session, address):
+    if address.startswith('catchall-'):
+        debug_msg(f'Outbound message from catch all to {address}')
+        recipient_info = CatchAllInfo(address)
+        if not recipient_info.is_valid:
+            return 'proceed'
         
-        try:
-            (outbound_catchall_addr, outbound_recipient_display_name, outbound_recipient_username, outbound_recipient_domain) = (part.decode('utf-8') for part in catchall_reply_information)
-            debug_msg(f'Catchall information: FROM: {outbound_catchall_addr}; DISP: {outbound_recipient_display_name}; TO: {outbound_recipient_username}@{outbound_recipient_domain}')
-            (outbound_catchall_user, outbound_catchall_domain) = outbound_catchall_addr.split('@')
-        except ValueError:
-            # What will happen will happen
-            session['action'] = MessageHandlerAction.PASS
-            return
-        
-        session['outbound_recipient'] = email.headerregistry.Address(display_name=outbound_recipient_display_name, username=outbound_recipient_username, domain=outbound_recipient_domain)
-        session['outbound_sender'] = email.headerregistry.Address(display_name='', username=outbound_catchall_user, domain=outbound_catchall_domain)
-        session['action'] = MessageHandlerAction.MODIFY_OUTBOUND
+        debug_msg(f'Rewriting envelope recipient: {recipient_info.recipient_addr}')
+        return f'rewrite|<{recipient_info.recipient_addr}>'
     else:
-        debug_msg(f'Received message for catchall recipient {address} (mapped to {catchall_recipient_map.get("address")})')
-        session['catchall_recipient'] = address
-        session['local_recipient'] = catchall_recipient_map.get('address')
-        session['action'] = MessageHandlerAction.MODIFY_INBOUND
+        return 'proceed'
 
 def modify_inbound_recipients(session, lines):
     local_recipient = session.get('local_recipient')
     catchall_recipient = session.get('catchall_recipient')
     if local_recipient is None or catchall_recipient is None:
         return lines
-    
     
     # The default email policy, email.policy.default, parses the entire message as opposed to
     # the compat32 policy which only parses the header and is used by default
@@ -103,17 +195,17 @@ def modify_inbound_recipients(session, lines):
     if 'reply-to' in parsed_message:
         # According to RFC2822, there should only be one reply-to address if it exists,
         # so we will only work on the first one we find
-        original_reply_to = parsed_message['reply-to'].address
+        original_reply_to = parsed_message['reply-to'].addresses[0]
         catchall_reply_to = b'\x1e'.join([catchall_recipient.encode(), original_reply_to.display_name.encode(), original_reply_to.username.encode(), original_reply_to.domain.encode()])
         debug_msg(f'Rewriting reply to address from {original_reply_to} to {base64.b64encode(catchall_reply_to).decode("utf-8")}')
-        new_reply_to = email.headerregistry.Address(display_name='', username=base64.b64encode(catchall_reply_to).decode('utf-8'), domain='catchall.invalid')
+        new_reply_to = email.headerregistry.Address(display_name='', username='catchall-' + base64.b64encode(catchall_reply_to).decode('utf-8'), domain=original_reply_to.domain)
         parsed_message.replace_header('reply-to', new_reply_to)
     else:
-        original_from = parsed_message['from'].address
+        original_from = parsed_message['from'].addresses[0]
         catchall_reply_to = b'\x1e'.join([catchall_recipient.encode(), original_from.display_name.encode(), original_from.username.encode(), original_from.domain.encode()])
         catchall_reply_to_b64 = base64.b64encode(catchall_reply_to).decode('utf-8')
-        debug_msg(f'Adding reply-to address {catchall_reply_to_b64}@catchall.invalid to message')
-        parsed_message.add_header('reply-to', f'{catchall_reply_to_b64}@catchall.invalid')
+        debug_msg(f'Adding reply-to address catchall-{catchall_reply_to_b64}@{original_from.domain} to message')
+        parsed_message.add_header('reply-to', f'catchall-{catchall_reply_to_b64}@{original_from.domain}')
     
     # Return the edited message
     return parsed_message.as_string().strip().split('\n')
@@ -166,11 +258,13 @@ def start():
     # Register a handler for the 'tx-rcpt' event which parses out the recipient
     server.register_handler('report', 'tx-rcpt', extract_recipient)
 
+    server.register_handler('filter', 'rcpt-to', rewrite_envelope)
+
     # Register the rewrite_message_recipients filter
     server.register_message_filter(lambda session, lines: rewrite_message_recipients(session, lines))
 
     # Go!
     server.serve_forever()
 
-if __name__ == 'main':
+if __name__ == '__main__':
     start()
